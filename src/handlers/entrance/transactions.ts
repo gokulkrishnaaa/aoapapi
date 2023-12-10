7ae043b3873c9cb5eb411b2f35dad0950f970946
Raju George <test@example.com>
2023-12-10 15:20:03 +0530

    modified transaction to be viewed by role

--- src/handlers/entrance/transactions.ts
+++ src/handlers/entrance/transactions.ts
@@ -4,16 +4,28 @@ import { BadRequestError } from "../../errors/bad-request-error";
 import { CannotProcessError } from "../../errors/cannot-process-error";
 
 export const getTransactionsByApplication = async (req, res) => {
+  const { role } = req.currentUser;
   const examapplicationId = req.params.id;
 
   if (!examapplicationId) {
     throw new BadRequestError("Input is invalid");
   }
 
+  let whereClause: {
+    examapplicationId: string;
+    type?: "ONLINE" | "AGENT";
+  } = {
+    examapplicationId,
+  };
+
+  if (role === "candidate") {
+    whereClause.type = "ONLINE";
+  } else if (role === "agent") {
+    whereClause.type = "AGENT";
+  }
+
   const entrancePayments = await prisma.entrancePayments.findMany({
-    where: {
-      examapplicationId,
-    },
+    where: whereClause,
     orderBy: {
       createdAt: "desc", // Sort by createdAt in descending order (latest to oldest)
     },
@@ -28,6 +40,7 @@ export const getTransactionsByCandidate = async (req, res) => {
   const entrancePayments = await prisma.entrancePayments.findMany({
     where: {
       candidateId: id,
+      type: "ONLINE",
     },
     orderBy: {
       createdAt: "desc", // Sort by createdAt in descending order (latest to oldest)
