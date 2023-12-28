8bc135649fba7e2ce506970557c5057cd1bb426f
Raju George <test@example.com>
2023-12-28 21:56:52 +0530

    pearson apis updated

--- src/handlers/admin/vendor/examcenter.ts
+++ src/handlers/admin/vendor/examcenter.ts
@@ -263,12 +263,11 @@ export const verifyingAllCandidatesWorker = async (data) => {
       }
 
       const headers = {
-        TokenID: "U@TUFnhDy6gur9B4",
+        TokenID: process.env.CBT_USERSYNC_TOKEN,
         "Content-Type": "application/json",
       };
 
-      const apiUrl =
-        "https://uat-pearsonvue.excelindia.com/AmritaSchedulerAPI/User/SyncUser";
+      const apiUrl = process.env.CBT_USERSYNC_URL;
 
       try {
         const { data } = await axios.post(apiUrl, postData, { headers });
