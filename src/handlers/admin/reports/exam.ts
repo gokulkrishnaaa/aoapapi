b30a19d69d88af4d982470b41203a64fe360e5f8
gokulnfonics <gokul@nfonics.com>
2024-01-10 19:11:46 +0530

    AOAP-42 Adding Columns in Admin Report modified the function getExamRegisteredReport and added the column Applied

--- src/handlers/admin/reports/exam.ts
+++ src/handlers/admin/reports/exam.ts
@@ -8,10 +8,12 @@ export const getExamRegisteredReport = async (req, res) => {
         ent.name AS entrance_name,
         e.id AS exam_id,
         e.description AS exam_description,
-        COUNT(r.id) AS registration_count
+ 	COUNT(ea.id) AS exam_application_count,
+        COUNT(reg.id) AS registration_count
     FROM "Exam" e
     LEFT JOIN "Entrance" ent ON e."entranceId" = ent.id
-    LEFT JOIN "Registration" r ON e.id = r."examId"
+    LEFT JOIN "ExamApplication" ea ON e.id = ea."examId"
+    LEFT JOIN "Registration" reg ON ea.id = reg."examapplicationId"
     GROUP BY ent.name, e.id, e.description;
     `;
 
@@ -22,6 +24,7 @@ export const getExamRegisteredReport = async (req, res) => {
       entrance: row.entrance_name,
       examId: row.exam_id,
       exam: row.exam_description,
+      applied:Number(row.exam_application_count),
       registered: Number(row.registration_count),
     }));
 
